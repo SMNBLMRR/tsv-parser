18e7f9b9a762ddbafe442cc50c08514843603c40
Simone, Marrandino <simone.marrandino@gft.com>
2023-10-07 13:50:41 +0200

    tsv-parser

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # tsv-parser
+
+`tsv-parser` can convert TSV into JSON 
+
+## Install
+
+```console
+$ npm install tsv-parser
+```
+
+Using yarn:
+
+```console
+$ yarn add tsv-parser
+```
+
+## Example
+Begin by setting up a readable stream to the TSV file you intend to parse. Once done, initiate `TsvParser` and direct the stream to flow through it.
+
+Imagine you're working with a TSV file named `data.tsv`. The content might look something like this:
+
+```
+NAME\tAGE
+Daffy Duck\t24
+Bugs Bunny\t22
+```
+
+Here's how you'd extract and display the information using TsvParser:
+
+```js
+const { createReadStream } = require("node:fs");
+const TsvParser = require("tsv-parser");
+
+let source = createReadStream("data.tsv");
+let data = [];
+
+source
+  .pipe(TsvParser())
+  .on("data", (record) => {
+    data.push(record);
+  })
+  .on("end", () => {
+    console.log(data);
+  });
+```
+
+When you run the above script, you can expect an output resembling:
+```
+[
+    { NAME: 'Daffy Duck', AGE: '24' }
+    { NAME: 'Bugs Bunny', AGE: '22' }
+]
+```
+
+## Options
+Type: `Object`
+#### headers
+
+Type: `Array[String] | Boolean`
+
+The headers option determines how the TSV rows are mapped to object properties. If you don't provide a specific headers option, the tsv-parser assumes that the first row in your TSV file contains the headers.
+
+By setting the headers option to false, you're telling the parser that the first row doesn't contain headers. Instead, the parser will use the column indices as keys.
---
+++ test.js
@@ -0,0 +1,82 @@
+const { Readable } = require("node:stream");
+const TsvParser = require("./index");
+const t = require("tap");
+
+t.test("TsvParser test", t => {
+    t.plan(1);
+    let result = [];
+    const sampleInput = 'name\tage\tcity\nJohn\t30\tNew York\nJane\t25\tLondon';
+    const expectedOutput = [
+        { name: 'John', age: '30', city: 'New York' },
+        { name: 'Jane', age: '25', city: 'London' }
+    ];
+
+    const readable = new Readable({
+        read(){
+            this.push(sampleInput);
+            this.push(null);
+        }
+    })
+
+    readable.pipe(TsvParser())
+    .on("data",chunk => {
+        result.push(chunk);
+    }).on("end", () => {
+        t.same(result,expectedOutput,"should parse TSV correctly")
+    })
+})
+
+
+t.test("Custom header", t => {
+    t.plan(1);
+    let result = [];
+    const sampleInput = 'name\tage\tcity\nJohn\t30\tNew York\nJane\t25\tLondon';
+    const expectedOutput = [
+        { A: 'name', B: 'age', C: 'city' },
+        { A: 'John', B: '30', C: 'New York' },
+        { A: 'Jane', B: '25', C: 'London' }
+    ];
+
+    const readable = new Readable({
+        read(){
+            this.push(sampleInput);
+            this.push(null);
+        }
+    })
+
+    readable
+    .pipe(TsvParser({headers:["A","B","C"]}))
+    .on("data", chunk => {
+        result.push(chunk);
+    })
+    .on("end", () => {
+        t.same(result,expectedOutput,"should parse TSV correctly with custom header")
+    })
+})
+
+t.test("Headers false", t => {
+    t.plan(1);
+    let result = [];
+    const sampleInput = 'name\tage\tcity\nJohn\t30\tNew York\nJane\t25\tLondon';
+    const expectedOutput = [
+        { "0": 'name', "1": 'age', "2": 'city' },
+        { "0": 'John', "1": '30', "2": 'New York' },
+        { "0": 'Jane', "1": '25', "2": 'London' }
+    ];
+
+    const readable = new Readable({
+        read(){
+            this.push(sampleInput);
+            this.push(null);
+        }
+    })
+
+    readable
+    .pipe(TsvParser({headers:false}))
+    .on("data", chunk => {
+        result.push(chunk);
+    })
+    .on("end", () => {
+        t.same(result,expectedOutput,"should parse TSV correctly with custom header")
+    })
+})
