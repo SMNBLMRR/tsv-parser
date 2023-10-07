d4b29a9e7bad2435268bda4dd413b0039739df94
Simone, Marrandino <simone.marrandino@gft.com>
2023-10-07 14:59:11 +0200

    prettier

--- index.js
+++ index.js
@@ -5,43 +5,43 @@ module.exports = TsvParser;
 module.exports.default = TsvParser;
 module.exports.TsvParser = TsvParser;
 
-function TsvParser(options = {}){
-    function isSameLength(line,header){
-        return line?.length === header?.length;
-    }
+function TsvParser(options = {}) {
+  function isSameLength(line, header) {
+    return line?.length === header?.length;
+  }
 
-    function buildObject(line){
-        let headers = (options?.headers === false || !isSameLength(line,options.headers)) ? line?.map((_,idx) => idx) : options.headers;
-        let obj = {};
-        for(let i = 0; i <line.length; i++){
-            let header = headers[i];
-            if(options?.headers === null) return obj;
-            if(options?.headers !== undefined){
-                obj[header] = line[i];
-            }
-        }
-        return obj;
+  function buildObject(line) {
+    let headers = options?.headers === false || !isSameLength(line, options.headers) ? line?.map((_, idx) => idx) : options.headers;
+    let obj = {};
+    for (let i = 0; i < line.length; i++) {
+      let header = headers[i];
+      if (options?.headers === null) return obj;
+      if (options?.headers !== undefined) {
+        obj[header] = line[i];
+      }
     }
+    return obj;
+  }
 
-    function groupLine(data){
-        const lines = data.split(/\r?\n|\r/);
-        if(options.headers === null || options.headers === undefined){
-            let firstLine = lines.splice(0,1);
-            options.headers = firstLine[0].split("\t");
-        }
-        const chunk = lines.map(line => buildObject(line.split('\t')));
-        return chunk;
+  function groupLine(data) {
+    const lines = data.split(/\r?\n|\r/);
+    if (options.headers === null || options.headers === undefined) {
+      let firstLine = lines.splice(0, 1);
+      options.headers = firstLine[0].split("\t");
     }
-    
-    return new Transform({
-        objectMode:true,
-        transform(row,_encoding,callback){
-            let data = row.toString();
-            let cell = groupLine(data);
-            for(let c of cell){
-                this.push(c);
-            }
-            callback();
-        }
-    }
-)}
+    const chunk = lines.map((line) => buildObject(line.split("\t")));
+    return chunk;
+  }
+
+  return new Transform({
+    objectMode: true,
+    transform(row, _encoding, callback) {
+      let data = row.toString();
+      let cell = groupLine(data);
+      for (let c of cell) {
+        this.push(c);
+      }
+      callback();
+    },
+  });
+}
